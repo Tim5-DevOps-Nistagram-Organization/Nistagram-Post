752759e3e4ad5d698bd81d3482ff50c0668ed097
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-06 17:35:04 +0200

    pipeline for pull request #13

--- src/test/java/rs/ac/uns/ftn/devops/tim5/nistagrampost/NistagramPostApplicationTests.java
+++ src/test/java/rs/ac/uns/ftn/devops/tim5/nistagrampost/NistagramPostApplicationTests.java
@@ -3,11 +3,16 @@ package rs.ac.uns.ftn.devops.tim5.nistagrampost;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest
 class NistagramPostApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        int a = 1, b = 1;
+        int sum = a + b;
+        assertEquals(2, sum);
+    }
 
 }
