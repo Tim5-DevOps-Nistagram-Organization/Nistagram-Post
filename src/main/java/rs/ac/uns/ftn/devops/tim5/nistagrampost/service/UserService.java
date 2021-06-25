086300da07b2d77c81dfc1299037a966f62bbb16
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-25 20:56:22 +0200

    User registered saga #20

--- src/main/java/rs/ac/uns/ftn/devops/tim5/nistagrampost/service/UserService.java
+++ src/main/java/rs/ac/uns/ftn/devops/tim5/nistagrampost/service/UserService.java
@@ -6,4 +6,8 @@ import rs.ac.uns.ftn.devops.tim5.nistagrampost.model.User;
 public interface UserService {
 
     User findByUsername(String username) throws ResourceNotFoundException;
+    
+    void create(String username, String email);
+
+    void delete(String username);
 }
--- src/main/java/rs/ac/uns/ftn/devops/tim5/nistagrampost/service/impl/UserServiceImpl.java
+++ src/main/java/rs/ac/uns/ftn/devops/tim5/nistagrampost/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package rs.ac.uns.ftn.devops.tim5.nistagrampost.service.impl;
 
 import org.springframework.stereotype.Service;
 import rs.ac.uns.ftn.devops.tim5.nistagrampost.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import rs.ac.uns.ftn.devops.tim5.nistagrampost.model.User;
 import rs.ac.uns.ftn.devops.tim5.nistagrampost.repository.UserRepository;
 import rs.ac.uns.ftn.devops.tim5.nistagrampost.service.UserService;
@@ -11,6 +12,7 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
+    @Autowired
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
@@ -18,5 +20,16 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findByUsername(String username) throws ResourceNotFoundException {
         return userRepository.findByUsername(username).orElseThrow(() -> new ResourceNotFoundException("User"));
+    }
+    
+    public void create(String username, String email) {
+        User user = new User(username, email);
+        User userSaved = userRepository.save(user);
+    }
+
+    @Override
+    public void delete(String username) {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new ResourceNotFoundException("User"));
+        userRepository.delete(user);
     }
 }
